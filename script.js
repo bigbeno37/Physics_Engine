2532550ac96385af8f75a4489b52a08ab0e3d783
bigbeno37 <bigbeno37@gmail.com>
2015-04-09 20:54:30 +1000

    Physics engine is running nicely, though different framerates cause issues

--- script.js
+++ script.js
@@ -1,78 +1,109 @@
-var heightOfBall = 200;
+var heightOfBall = 850;
 var angleOfBall = 45;
-var velocity = 250;
-var gravity = 200;
+// Javascript requires degrees to be turned into radians to be able to compute them, so I just convert it once and use this instead
+var angleOfBallRadians = convertToRadians(angleOfBall);
+var velocity = 300;
+// As explained, velocity is a vector; therefore, we need to determine it's x and y components using trig
+var velocityX = velocity * Math.cos(angleOfBallRadians);
+var velocityY = velocity * Math.sin(angleOfBallRadians);
+var gravity = 9.8;
+// I made it so that the custom coded renderer can utilise multiple framerates
 var framerate = 60;
+// Due to a function requiring a millisecond time, I convert the amount of frames per second to millisecond per frame
 var milliFramerate = 1000 / framerate;
 
 var currentTime = 0;
+var currentPositionX = 0;
+var currentPositionY = heightOfBall;
 
-var ball = null;
-var canvas = null;
+var ballIsDrawn = false;
 
-function getScreenHeight(){
-	return ($(window).height()-2);
-}
+var sizeOfCanvasX = 700;
+var sizeOfCanvasY = 900;
 
-function getScreenWidth(){
-	return ($(window).width()-2);
-}
+var ball;
+var canvas;
 
 function drawBall(){
+	// This is where the framework comes in; it's called fabric.js, and it handles the rendering of the ball only. I make it move	
 	ball = new fabric.Circle({
-	  top: (getScreenHeight() - heightOfBall),
+	  // Fabric.js uses a co-ordinate system starting from the top-right
+	  // Therefore, we specify the amount of whitespace required to get the ball to heightOfBall pixels up
+	  // Height + Whitespace = Size of Canvas, hence Whitespace = Size of Canvas - Height
+	  top: (sizeOfCanvasY - heightOfBall),
 	  fill: 'red',
 	  radius: 10
 	});
 
-	// "add" rectangle onto canvas
 	canvas.add(ball);
 	canvas.renderAll();
+
+	ballIsDrawn = true;
 }
 
 function convertToRadians(degrees){
 	return (degrees * (Math.PI / 180)); 
 }
 
-function determinePhi(time){
-	//return (velocity * time) - ((gravity * (time * time))/2)
-
-	return ((velocity*Math.sin(convertToRadians(angleOfBall))) - (gravity*time));
-}
-
-function determineDistance(time){
-	/*var tanBallRadians = Math.tan(convertToRadians(angleOfBall));
-	var distance = determinePhi(time);
-
-	return (((-1 * gravity * tanBallRadians)/(Math.pow(velocity, 2))) * Math.pow(distance, 2)) + (tanBallRadians * distance) + heightOfBall;*/
-
-	return velocity*Math.cos(convertToRadians(angleOfBall))*time;
-}
-
-function renderFrame(){
-	currentTime += milliFramerate;
-	var time = convertToSeconds(currentTime);
-	var topValue = 200 + (-1*determinePhi(time));
-	var leftValue = determineDistance(time);
-
-	ball.set({top: topValue, left: leftValue});
-	canvas.renderAll();
-
-	console.log("Current time is " + currentTime + "ms, " + time + "s");
-	console.log("Top is: " + topValue + " and left is: " + leftValue);
-}
-
 function convertToSeconds(milliTime){
 	return milliTime*0.001;
 }
 
+function determineX(){
+	// Every second, the ball moves Vx pixels
+	// Every 1/2 second, the ball moves Vx / 2 pixels
+	// Every 1/4 second, the ball moves Vx / 4 pixels
+	// Continuing with this trend, every frame this ball will move Vx / framerate pixels onto it's previous position
+	return currentPositionX + (velocityX / framerate);
+}
+
+
+function determineY(time){
+	// The same principle as above applies here, but in the y direction, the force of gravity is applied
+	// Because gravity is an acceleration, it will increase with time
+	// Due to gravity's 'pulling' on the ball, it will 'eat' away at the ball's Vy component
+	// After some time, the ball's Vy component will be equal to the gravity, and the ball will be brought down
+	return currentPositionY + ((velocityY / framerate) - (gravity*time));
+}
+
+function renderFrame(){
+	currentTime += milliFramerate;
+	// For the above functions (determineX and Y), to determine correct placements requires a time in seconds; hence, we convert milliseconds to seconds
+	var time = convertToSeconds(currentTime);
+
+	var topValue = sizeOfCanvasY - determineY(time)
+	var leftValue = determineX();
+
+	if(!ballIsDrawn){
+		drawBall();
+	}else{
+		ball.set({top: topValue, left: leftValue});
+		canvas.renderAll();
+	}
+
+	currentPositionY = determineY(time);
+	currentPositionX = leftValue;
+
+	// These spurt out constant information that I can use to debug
+	console.log("Current time is " + currentTime + "ms, " + time + "s");
+	console.log("Top is: " + topValue + " and left is: " + leftValue);
+}
+
 $(document).ready(function(){
-	$("#c").attr({"width": getScreenWidth(), "height": getScreenHeight()});
+	// More debug information, just to check my maths
+	console.log(angleOfBall + " to radians (according to Javascript) is " + angleOfBallRadians);
+	console.log("The velocity vector is " + velocity + "; velocityX is: " + velocityX + "; velocityY is: " + velocityY);
+
+	// Initialises Fabric.js and its functions
+	$("#c").attr({"width": sizeOfCanvasX, "height": sizeOfCanvasY});
 	canvas = new fabric.StaticCanvas('c');
 	drawBall();	
 
+	// Every milliFramerate (see why I needed to convert the framerate?), run renderFrame()
+	// I store this function as a variable, so that I can later end it
 	var renderBallMovement = setInterval(function(){ renderFrame() }, milliFramerate);
 
+	// This function will run after 10000ms, or, 10 seconds
 	setTimeout(function(){
 		clearInterval(renderBallMovement);
 	}, 10000);
